ebbf877d8c886a52882926f37809cc10410a503f
SNOODBOOK\MiniSnood <snoodleking@gmail.com>
2017-05-11 21:31:45 -0400

    created and hooked up pseudo views for the hangman game added random int generator

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,17 +8,50 @@ window.addEventListener( "keydown", function( e )
 	hangMan.checkKeyInput( e );
 });
 
+//start game after page load
+window.addEventListener( "load", function()
+{
+    console.log( "document loaded" );
+    //init hangman game with starting values and pass in views
+    hangMan.init
+    (
+        document.querySelector( "#current-word-view" ),
+        document.querySelector( "#guessed-letter-view" )
+    );
+});
+
 //define hangman game object
 var hangMan = 
 {
+    //game variables
     isEnabled: false,
     maxTurns: 12,
     currentTurn: 0,
+    currentWord: "",
+
+    //views
+    currentWordView: null,
+    guessedLettersView: null,
+
+    //data
+    possibleWords: ["one", "two", "three"],
+    guessedLetters: [],
+    currentLetters: [],
+
 
     //initialize the game
-    init: function()
+    init: function( tCurrentWordView, tGuessedLettersView )
     {
+        //establish data
     	this.currentTurn = this.maxTurns;
+        this.currentWord = this.possibleWords[ this.getRandomInt( 0, this.possibleWords.length ) ];
+
+        //hook up views
+        this.currentWordView = tCurrentWordView;
+        this.guessedLettersView = tGuessedLettersView;
+
+        console.log( "current word = " + this.currentWord );
+        console.log( this.currentWordView );
     },
 
     //check what button was pressed
@@ -42,8 +75,12 @@ var hangMan =
     		alert( "you lose" );
     	}
     },
-};
 
-//init hangman game with starting values
-hangMan.init();
+    getRandomInt: function ( min, max ) 
+    {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor( Math.random() * ( max - min ) ) + min;
+    }
+};
 
